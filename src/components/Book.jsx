30a292f0319f0f41475062ad1b0660876554ffdf
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-02-18 22:44:36 -0700

    tweaking book card styling

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -11,24 +11,26 @@ export default function Book({ book }) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white w-full  mt-7 mx-auto py-7 px-4 text-center rounded-md shadow-md">
-      <div className="inline">
+    <div className="relative flex flex-col items-center justify-center bg-white w-full  mt-7 mx-auto pt-6 pb-32 px-4 text-center rounded-md shadow-md">
+      <div className="max-h-48 overflow-hidden">
         <img className="w-32 m-3" src={image} alt="book cover" />
       </div>
       <h3 className="text-gray-900 font-extrabold mt-3">{book.title}</h3>
       <h4 className="text-gray-600 mt-2">{book.author}</h4>
-      <button
-        type="button"
-        className="bg-booklistRed text-white py-1 px-3 rounded-3xl font-semibold mt-3.5 mb-10 shadow-md hover:bg-booklistRed-light hover:-translate-y-0.5 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-booklistRed focus:ring-opacity-50 active:bg-booklistRed-dark"
-      >
-        Genre
-      </button>
-      <div className="flex items-center justify-between w-52 font-bold">
-        <img className="h-4" src={bookmark} alt="bookmark icon" />
-        <p>Bookmark</p>
-        <p> | </p>
-        <p>Page:</p>
-        <p>{book.pages}</p>
+      <div className="absolute bottom-0 w-full">
+        <button
+          type="button"
+          className="bg-booklistRed text-white py-1 px-3 rounded-3xl font-semibold mt-3.5 mb-10 shadow-md hover:bg-booklistRed-light hover:-translate-y-0.5 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-booklistRed focus:ring-opacity-50 active:bg-booklistRed-dark"
+        >
+          Genre
+        </button>
+        <div className=" pb-4 px-6 w-full flex items-center justify-between font-bold">
+          <img className="h-4" src={bookmark} alt="bookmark icon" />
+          <span>Bookmark</span>
+          <span>|</span>
+          <span>pages:</span>
+          <span>{book.pages}</span>
+        </div>
       </div>
     </div>
   );

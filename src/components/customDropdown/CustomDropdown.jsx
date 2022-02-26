755f47b39c2cc388dc66a4745d072eaf012f7e60
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-02-25 21:19:56 -0700

    work on dropdown

--- src/components/customDropdown/CustomDropdown.jsx
+++ src/components/customDropdown/CustomDropdown.jsx
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 import SelectList from './SelectList';
 
-function Select({ name, searchBooks, bookSelection, autofillBookInfo }) {
+function Select({
+  name,
+  searchBooks,
+  bookSelection,
+  setBookSelection,
+  autofillBookInfo,
+}) {
   const [toggle, setToggle] = useState(false);
   const [selected, setSelected] = useState({});
   const [selectInput, setSelectInput] = useState('');
 
   function handleSelect(e, book) {
     setToggle(true);
+    if (selectInput.length < 3) {
+      setBookSelection([]);
+      setToggle(false);
+    }
     if (e.type === 'click' || e.key === 'Enter') {
       autofillBookInfo(book);
       setSelected(book);
@@ -16,42 +26,54 @@ function Select({ name, searchBooks, bookSelection, autofillBookInfo }) {
       setToggle(false);
     }
   }
+  // closes select if the user clicks anywhere outside the dropdown
+  function outsideClickHandler() {
+    setToggle(false);
+    document.querySelector('#selectLabel').focus();
+  }
 
   return (
-    <div className="relative mt-1">
-      <input
-        id="selectLabel"
-        name={name}
-        value={selectInput}
-        autoComplete="off"
-        onChange={(e) => {
-          setSelectInput(e.target.value);
-          handleSelect(e);
-          searchBooks(selectInput);
-        }}
-        className={`relative z-10 flex items-center justify-center w-full py-1.5 px-4  border-2 ${
-          toggle ? ' rounded-t-md' : 'rounded-md'
-        }`}
-        placeholder="Search"
-        type="text"
-        onClick={() => {
-          if (bookSelection.length > 0) setToggle(!toggle);
-        }}
+    <div>
+      <div
+        className={`absolute inset-0 z-40 ${!toggle ? 'hidden' : 'block'} `}
+        onClick={outsideClickHandler}
+        role="dialog"
       />
-      <ul
-        role="listbox"
-        aria-labelledby="selectLabel"
-        style={{ top: '27px', maxHeight: '600px' }}
-        className={`absolute w-full pt-3 bg-white flex-col justify-center overflow-y-scroll  items-center ${
-          toggle ? ' rounded-b-xl' : 'rounded-xl hidden'
-        }`}
-      >
-        <SelectList
-          selected={selected}
-          bookSelection={bookSelection}
-          handleSelect={handleSelect}
+      <div className="relative z-50 mt-1">
+        <input
+          id="selectLabel"
+          name={name}
+          value={selectInput}
+          autoComplete="off"
+          onChange={(e) => {
+            setSelectInput(e.target.value);
+            handleSelect(e);
+            searchBooks(selectInput);
+          }}
+          className={`relative z-10 flex items-center justify-center w-full py-1.5 px-4  border-2 ${
+            toggle ? ' rounded-t-md' : 'rounded-md'
+          }`}
+          placeholder="Search"
+          type="text"
+          onClick={() => {
+            if (bookSelection?.length > 0) setToggle(!toggle);
+          }}
         />
-      </ul>
+        <ul
+          role="listbox"
+          aria-labelledby="selectLabel"
+          style={{ top: '27px', maxHeight: '600px' }}
+          className={`absolute w-full pt-3 bg-white flex-col justify-center overflow-y-scroll  items-center ${
+            toggle ? ' rounded-b-xl' : 'rounded-xl hidden'
+          }`}
+        >
+          <SelectList
+            selected={selected}
+            bookSelection={bookSelection}
+            handleSelect={handleSelect}
+          />
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -149,6 +149,7 @@ export default function AddBookPage() {
                 searchBooks={searchBooks}
                 bookSelection={bookSelection}
                 autofillBookInfo={autofillBookInfo}
+                setBookSelection={setBookSelection}
               />
             </label>
 

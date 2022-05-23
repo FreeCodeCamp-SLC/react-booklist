d2e82c0eaba1a0b399fec717956afe76e97140cd
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-05-22 20:37:27 -0600

    work in progress

--- src/config.js
+++ src/config.js
@@ -1,2 +1,14 @@
+import axios from 'axios'
+import { loadAuthToken } from "./utils/local-storage";
+
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-export default API_BASE_URL;
+
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        Authorization: `Bearer ${loadAuthToken()}`,
+    }
+})
+
+export default api
--- src/hooks/useBooksApi.js
+++ src/hooks/useBooksApi.js
@@ -1,16 +1,6 @@
-import axios from 'axios';
 import { useQuery } from 'react-query';
-import API_BASE_URL from "../config";
-import { loadAuthToken } from "../utils/local-storage";
+import api from '../config'
 
-
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-      Authorization: `Bearer ${loadAuthToken()}`,
-  }
-})
 
 export default function useBooksApi() {
 

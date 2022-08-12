0272a77e4f0d776b07b5635b41c627cf49ef113b
Michael Corey <mcoreycoder@Michaels-MacBook-Pro.local>
2022-08-12 16:04:36 -0600

    updated Profile to keep in style with other pages, updated Header to add link to Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { loadAuthToken } from '../utils/local-storage';
+import React, { useEffect, useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { loadAuthToken } from '../utils/local-storage'
+import Header from '../components/Header'
 
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
+  const { user, isAuthenticated } = useAuth0()
+  const [userMetadata, setUserMetadata] = useState(null)
 
   useEffect(() => {
     const getUserMetadata = async () => {
-      const domain = 'utahfcc.us.auth0.com';
-      const accessToken = loadAuthToken();
+      const domain = 'utahfcc.us.auth0.com'
+      const accessToken = loadAuthToken()
       try {
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`
         const metadataResponse = await fetch(userDetailsByIdUrl, {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
-        });
-        const { userMetadataResponse } = await metadataResponse.json();
-        setUserMetadata(userMetadataResponse);
+        })
+        const { userMetadataResponse } = await metadataResponse.json()
+        setUserMetadata(userMetadataResponse)
       } catch (e) {
-        console.log(e.message);
+        console.log(e.message)
       }
-    };
-    getUserMetadata();
-  }, []);
+    }
+    getUserMetadata()
+  }, [])
 
   return (
     isAuthenticated && (
-      <section className="p-10 bg-gray-100">
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <h3>User Metadata</h3>
-        {userMetadata ? (
-          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-        ) : (
-          'No user metadata defined'
-        )}
+      <section className=' sm:grid grid-cols-layout grid-rows-layout'>
+        <Header />
+        <section className='p-10 bg-gray-100'>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <h3>User Metadata</h3>
+          {userMetadata ? (
+            <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+          ) : (
+            'No user metadata defined'
+          )}
+        </section>
       </section>
     )
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile

2b05806755e64ad515657359e3bf8f2443ead8c5
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-08-12 21:28:12 -0600

    added extra auth loading page

--- src/hooks/useBooksApi.js
+++ src/hooks/useBooksApi.js
@@ -1,15 +1,11 @@
 import { useQuery, useMutation } from 'react-query';
 import api from '../config'
-import { loadAuthToken } from '../utils/local-storage';
-
 
 export default function useGetBooks(booksItemCount, pageNumber, sortBy) {
 
-
   const getAllBooks = api.get(`/books`,{booksItemCount, pageNumber, sortBy}) 
 
-    return useQuery(['books', pageNumber], () => getAllBooks,{
-      enabled: !!loadAuthToken(),
+    return useQuery(['books', pageNumber], async () => getAllBooks,{
     });
 }
 

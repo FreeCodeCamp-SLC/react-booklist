f071819a4ddda0e04d9cd456db767720c6b5c434
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-09-11 02:42:35 -0600

    added button wrapper to svgs

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -37,40 +37,51 @@ export default function Book({ book, lists, pageNumber }) {
       className="flex flex-col items-center relative justify-center bg-white w-full  mt-7 mx-auto pt-6 pb-4 px-6 text-center rounded-md shadow-md cursor-pointer focus:ring focus:ring-booklistBlue    focus:ring-opacity-50"
     >
       {book.favorite && (
-        <svg
+        <button
+          className="cardBtn focus:ring-booklistBlue focus:ring-opacity-50 focus:ring-2 absolute top-2 right-2"
+          type="button"
           onClick={() => favoriteBookHandler(false)}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-7 h-7 text-booklistRed absolute top-0 right-0 mt-4 mr-6 hover:-translate-y-0.5 transform transition hover:text-gray-400 cardBtn"
         >
-          <path
-            className="cardBtn"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7 text-booklistRed absolute top-0 right-0 mt-4 mr-6 hover:-translate-y-0.5 transform transition hover:text-gray-400 cardBtn"
+          >
+            <path
+              className="cardBtn"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+        </button>
       )}
       {!book.favorite && (
-        <svg
-          onClick={() => favoriteBookHandler(true)}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-7 h-7 text-gray-300 absolute top-0 right-0 mt-4 mr-6 hover:-translate-y-0.5 transform transition hover:text-booklistRed cardBtn"
+        <button
+          className="cardBtn focus:ring-booklistBlue focus:ring-opacity-50 focus:ring-2 absolute top-2 right-2"
+          type="button"
+          onClick={() => favoriteBookHandler(false)}
         >
-          <path
-            className="cardBtn"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
+          <svg
+            onClick={() => favoriteBookHandler(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7 text-gray-300 absolute top-0 right-0 mt-4 mr-6 hover:-translate-y-0.5 transform transition hover:text-booklistRed cardBtn"
+          >
+            <path
+              className="cardBtn"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+        </button>
       )}
       <div className="max-h-48 rounded-sm overflow-hidden">
         <img

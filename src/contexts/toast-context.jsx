542cc2fea929b26a647477216fef859f48a7892e
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-10-03 22:59:39 -0600

    Update toast-context.jsx

--- src/contexts/toast-context.jsx
+++ src/contexts/toast-context.jsx
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react';
 const ToastContext = createContext();
 
 export const ToastProvider = ({ children }) => {
+  const [toastFade, setToastFade] = useState(false);
   const [toastStatus, setToastStatus] = useState('');
   const [toastType, setToastType] = useState('');
   const [book, setBook] = useState({});

65b234d184c3dac4b18b630c226faba506316c2d
pbarrowbyu121 <pbarrowbyu@gmail.com>
2025-06-24 19:05:55 -0600

    created SearchIcon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import useProfile from '../hooks/useProfileApi';
 import logo from '../images/nav-logo-new.png';
+import SearchIcon from './icons/SearchIcon';
 
 export default function Header({ searchHandler, listName }) {
   const { pathname } = useLocation();
@@ -175,20 +176,7 @@ export default function Header({ searchHandler, listName }) {
             pathname === '/lists' ||
             pathname === '/favorites') && (
             <div className="w-full flex items-center">
-              <svg
-                className="h-5 px-2 stroke-current text-booklistBlue-dark"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
+              <SearchIcon />
               <form action="/" method="GET" className="w-full">
                 <label htmlFor="search">
                   <input
---
+++ src/components/icons/SearchIcon.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchIcon = () => (
+  <div>
+    <svg
+      className="h-5 px-2 stroke-current text-booklistBlue-dark"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="{2}"
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
+  </div>
+);
+
+export default SearchIcon;
